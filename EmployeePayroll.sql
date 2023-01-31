72239a3b7d78e4639c40804f63e8d9c840841d66
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-31 17:47:22 +0530

    creating database

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -1,2 +1,10 @@
 --UC-1 creating database
 create database EmployeeServices;
+--UC-2 creating Table
+create Table empoyee_payrolls
+(
+id int identity(1,1) primary key,
+name varchar(200) not null,
+salary float,
+startDate date
+)
