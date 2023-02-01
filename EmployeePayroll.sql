b1624624ad830bf4ee0e943f2c3d37ec3f9f050f
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-02-01 14:14:27 +0530

    UsingAggregateFunction

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -1,14 +1,14 @@
 --UC-3 Insert values in Table
 Insert into empoyee_payroll(name,salary,startDate) values
-('shweta',20000,'2023-03-12'),
-('Magesh',25000,'2023-04-18'),
+('shweta',20000,'2023-01-12'),
+('Magesh',25000,'2023-01-18'),
 ('Gayathri',10000,'2022-05-13'),
 ('Aruna',30000,'2022-08-19');
 --UC-4 Retrieve All data--
 select * from empoyee_payroll;
 ------- UC 5: Select Query using Cast() an GetDate() -------
 select salary from empoyee_payroll where name='Magesh';
-select salary from empoyee_payroll where startDate BETWEEN Cast('2022-12-20' as Date) and GetDate();
+select salary from empoyee_payroll where startDate BETWEEN Cast('2018-01-01' as Date) and GetDate();
 ------- UC 6: Add Gender Column and Update Table Values -------
 Alter table empoyee_payroll
 add Gender char(1);
